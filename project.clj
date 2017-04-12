95765abe206a9cba865e0376786a7f8f8418ef86
Christophe Grand <christophe@cgrand.net>
2017-04-12 14:24:26 +0200

    nrepl bridge

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.nrepl "0.2.12"]]
+  :profiles {:dev {:dependencies [[com.taoensso/timbre "4.8.0"]]}})
---
+++ src/unrepl/nrepl_bridge.clj
@@ -0,0 +1,70 @@
+(ns unrepl.nrepl-bridge
+  (:require [clojure.tools.nrepl :as nrepl]
+    [clojure.tools.nrepl.transport :as t])
+  (:use [clojure.tools.nrepl.misc :only (uuid)]))
+
+(defn writer [transport session]
+  (let [write (fn [s] 
+                (binding [*out* *err*] (prn '> s))
+                (t/send transport {:op :stdin :stdin s :id (uuid) :session session}))]
+    (proxy [java.io.Writer] []
+      (close [] (t/send transport {:op :close :id (uuid) :session session}))
+      (flush [])
+      (write
+        ([x]
+          (write (cond 
+                   (string? x) x
+                   (integer? x) (str (char x))
+                   :else (String. ^chars x))))
+        ([string-or-chars off len]
+          (when (pos? len)
+            (write (subs (if (string? string-or-chars) string-or-chars (String. ^chars string-or-chars))
+                     off (+ off len)))))))))
+
+(defn reader [transport session]
+  (let [vr (volatile! (java.io.StringReader. ""))
+        wait-ready (fn []
+                     (when-some [msg (t/recv transport)]
+                       (if-some [s (or (:out msg) (:err msg))]
+                         (vreset! vr (java.io.StringReader. (doto s (->> (prn '<)))))
+                         (if (some #{"session-closed"} (:status msg))
+                           (do
+                             (.close transport)
+                             (vreset! vr nil))
+                           (recur)))))]
+    (proxy [java.io.Reader] []
+      (close [] (t/send transport {:op :close :id (uuid) :session session}))
+      (read
+        ([] (if-some [^java.io.Reader r @vr]
+              (let [c (.read r)]
+                (if (neg? c)
+                  (do (wait-ready) (recur this))
+                  c))
+              -1))
+        ([cbuf]
+          (if-some [^java.io.Reader r @vr]
+            (let [c (if (instance? java.nio.CharBuffer cbuf)
+                      (.read r ^java.nio.CharBuffer cbuf)
+                      (.read r ^chars cbuf))]
+              (if (neg? c)
+                (do (wait-ready) (recur this cbuf))
+                c))
+              -1))
+        ([cbuf off len]
+          (if-some [^java.io.Reader r @vr]
+            (let [c (.read r cbuf off len)]
+              (if (neg? c)
+                (do (wait-ready) (recur this cbuf off len))
+                c))
+              -1))))))
+
+(defn repl
+  "Starts a plain repl over nrepl. Returns a map with two keys :output and :input.
+   The value under :input is a writer and the one under :output is a reader connected to the repl.
+   Takes the same arguments as nrepl/connect."
+  [& args]
+  (let [transport (apply nrepl/connect args)
+        session (-> transport (t/send {:op :clone :id (uuid)}) t/recv :new-session)]
+    (t/send transport {:op :eval :code "(clojure.main/repl)" :id (uuid) :session session})
+    {:input (writer transport session)
+     :output (reader transport session)}))
