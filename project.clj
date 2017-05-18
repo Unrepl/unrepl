ff04af97e4e44d77ed778a3a4bd5000179342532
Christophe Grand <christophe@cgrand.net>
2017-05-18 16:43:48 +0200

    Creates the unrepl-make-blob task and set it to run automatically

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.nrepl "0.2.12"]]
-  :profiles {:dev {:dependencies [[com.taoensso/timbre "4.8.0"]]}})
+  :profiles {:dev {:dependencies [[com.taoensso/timbre "4.8.0"]]}}
+  :prep-tasks ["unrepl-make-blob" "javac" "compile"])
---
+++ tasks/leiningen/unrepl_make_blob.clj
@@ -0,0 +1,21 @@
+(ns leiningen.unrepl-make-blob
+  (:require [clojure.java.io :as io]))
+
+(defn unrepl-make-blob [project]
+  (let [code (str (slurp "src/unrepl/print.clj") (slurp "src/unrepl/repl.clj") "\n(unrepl.repl/start)")]
+    (spit "resources/unrepl/blob.clj"
+      (prn-str
+        `(let [prefix# (name (gensym))
+             code# (.replaceAll ~code "unrepl\\.(?:repl|print)" (str "$0" prefix#))
+             rdr# (-> code# java.io.StringReader. clojure.lang.LineNumberingPushbackReader.)]
+         (try
+           (binding [*ns* *ns*]
+             (loop [ret# nil]
+               (let [form# (read rdr# false 'eof#)]
+                 (if (= 'eof# form#)
+                   ret#
+                   (recur (eval form#))))))
+           (catch Throwable t#
+             (println "[:unrepl.upgrade/failed]")
+             (throw t#))))))))
+
