ac18487b11164b56e3c4e43a2309ad9d93e7de45
Christophe Grand <christophe@cgrand.net>
2018-02-26 11:42:23 +0100

    no more code-in-a-big-string for the blob, instead transiently upgrade the repl

--- tasks/leiningen/unrepl_make_blob.clj
+++ tasks/leiningen/unrepl_make_blob.clj
@@ -82,26 +82,28 @@
         suffix (str "$" (-> code (.getBytes "UTF-8") sha1 java.io.ByteArrayInputStream. base64-encode))
         code (str/replace code #"(?<!:)unrepl\.(?:repl|print)" (fn [x] (str x suffix)))
         main-ns (symbol (str "unrepl.repl" suffix))]
-    (prn-str
-      `(or (find-ns '~main-ns)
-         (let [rdr# (-> ~code java.io.StringReader. clojure.lang.LineNumberingPushbackReader.)]
-          (try
-            (binding [*ns* *ns*]
-              (loop [ret# nil]
-                (let [form# (read rdr# false 'eof#)]
-                  (if (= 'eof# form#)
-                    ret#
-                    (recur (eval form#))))))
-            (catch Throwable t#
-              (println "[:unrepl.upgrade/failed]")
-              (throw t#))))))))
+    (str (strip-spaces-and-comments
+           (str
+             (prn-str
+               `(let [nop# (constantly nil)
+                      e# (atom (if (find-ns '~main-ns) nop# eval))]
+                  (clojure.main/repl
+                    :read (fn [rp# re#]
+                            (let [x# (read)] (case x# unrepl/fin re# x#)))
+                    :prompt nop#
+                    :eval (fn [x#] (@e# x#))
+                    :print nop#
+                    :caught (fn [e#] (set! *e e#) (reset! e# nop#)))
+                  (prn [:unrepl.upgrade/failed])))
+             code))
+     "\nunrepl/fin\n(ns user)(" main-ns "/start)\n")))
 
 (defn unrepl-make-blob
   ([project] (unrepl-make-blob project "resources/unrepl/blob.clj" "{}"))
   ([project target session-actions]
     (let [session-actions-source (if (re-find #"^\s*\{" session-actions) session-actions (slurp session-actions))
           session-actions-map (edn/read-string {:default (fn [tag data] (tagged-literal 'unrepl-make-blob-unquote (list 'tagged-literal (tagged-literal 'unrepl-make-blob-quote tag) data)))} session-actions-source)
-          code (str (slurp "src/unrepl/print.clj") (slurp "src/unrepl/repl.clj") "\n(ns user)\n(unrepl.repl/start)")]
+          code (str (slurp "src/unrepl/print.clj") (slurp "src/unrepl/repl.clj"))]
       (if (map? session-actions-map)
         (let [session-actions-map (into session-actions-map
                                     (map (fn [[k v]]
@@ -114,6 +116,6 @@
                                 (str/replace #"#unrepl-make-blob-(?:syntax|un)?quote " {"#unrepl-make-blob-syntaxquote " "`"
                                                                                         "#unrepl-make-blob-unquote " "~"
                                                                                         "#unrepl-make-blob-quote " "'"}))]
-          (spit target (str (strip-spaces-and-comments (gen-blob code session-actions)) "\n")))
+          (spit target (gen-blob code session-actions)))
         (println "The arguments must be: a target file name and an EDN map.")))))
 
