3ea60c98ff1d4566d63d0af172dfdb05aaa26dfc
Christophe Grand <christophe@cgrand.net>
2018-03-21 13:35:40 +0100

    make-blob now uses shade-libs

--- src/unrepl/make_blob.clj
+++ src/unrepl/make_blob.clj
@@ -2,36 +2,8 @@
   (:require
     [clojure.java.io :as io]
     [clojure.edn :as edn]
-    [clojure.string :as str]))
-
-(defn- base64-encode
-  "Non-standard base64 to avoid name munging"
-  [^java.io.InputStream in]
-  (let [table "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_$"
-        sb (StringBuilder.)]
-    (loop [shift 4 buf 0]
-      (let [got (.read in)]
-        (if (neg? got)
-          (do
-            (when-not (= shift 4)
-              (let [n (bit-and (bit-shift-right buf 6) 63)]
-                (.append sb (.charAt table n))))
-            #_(cond
-               (= shift 2) (.append sb "==")
-               (= shift 0) (.append sb \=))
-            (str sb))
-          (let [buf (bit-or buf (bit-shift-left got shift))
-                n (bit-and (bit-shift-right buf 6) 63)]
-            (.append sb (.charAt table n))
-            (let [shift (- shift 2)]
-              (if (neg? shift)
-                (do
-                  (.append sb (.charAt table (bit-and buf 63)))
-                  (recur 4 0))
-                (recur shift (bit-shift-left buf 6))))))))))
-
-(defn- sha1 [^bytes bytes]
-  (.digest (java.security.MessageDigest/getInstance "SHA-1") bytes))
+    [clojure.string :as str]
+    [unrepl.shade-libs :as shade]))
 
 (defn- strip-spaces-and-comments [s]
   #_(I had this nice #"(?s)(?:\s|;[^\n\r]*)+|((?:[^;\"\\\s]|\\.|\"(?:[^\"\\]|\\.)*\")+)"
@@ -84,20 +56,24 @@
         regular s))
     (str sb)))
 
-(defn- gen-blob [^String code session-actions]
+(defn- gen-blob [session-actions]
   (let [template (slurp (io/resource "unrepl/blob-template.clj"))
-        code (str/replace template "<BLOB-PAYLOAD>" code)
-        code (strip-spaces-and-comments code)
-        suffix (str "$" (-> code (.getBytes "UTF-8") sha1 java.io.ByteArrayInputStream. base64-encode))]
-    (str (str/replace code #"(?<!:)unrepl\.(?:repl|print)" (fn [x] (str x suffix))) "\n" session-actions "\n"))) ; newline to force eval by the repl
+        shaded-code-sb (StringBuilder.)
+        shaded-libs (shade/shade 'unrepl.repl
+                      {:writer (fn [_ ^String code] (.append shaded-code-sb code))
+                       :except [#"clojure\..*" 'unrepl.core]})
+        code (-> template
+               (str/replace "unrepl.repl"
+                 (str (shaded-libs 'unrepl.repl)))
+               (str/replace "<BLOB-PAYLOAD>" (str shaded-code-sb)))]
+    (str (strip-spaces-and-comments code) "\n" session-actions "\n"))) ; newline to force eval by the repl
 
 (defn -main
   ([] (-main "resources/unrepl/blob.clj" "{}"))
   ([target session-actions]
     (-> target io/file .getParentFile .mkdirs)
     (let [session-actions-source (if (re-find #"^\s*\{" session-actions) session-actions (slurp session-actions))
-          session-actions-map (edn/read-string {:default (fn [tag data] (tagged-literal 'unrepl-make-blob-unquote (list 'tagged-literal (tagged-literal 'unrepl-make-blob-quote tag) data)))} session-actions-source)
-          code (apply str (map #(slurp (io/resource %)) ["unrepl/core.clj" "unrepl/print.clj" "unrepl/repl.clj"]))]
+          session-actions-map (edn/read-string {:default (fn [tag data] (tagged-literal 'unrepl-make-blob-unquote (list 'tagged-literal (tagged-literal 'unrepl-make-blob-quote tag) data)))} session-actions-source)]
       (if (map? session-actions-map)
         (let [session-actions-map (into session-actions-map
                                     (map (fn [[k v]]
@@ -110,6 +86,6 @@
                                 (str/replace #"#unrepl-make-blob-(?:syntax|un)?quote " {"#unrepl-make-blob-syntaxquote " "`"
                                                                                         "#unrepl-make-blob-unquote " "~"
                                                                                         "#unrepl-make-blob-quote " "'"}))]
-          (spit target (gen-blob code session-actions)))
+          (spit target (gen-blob session-actions)))
         (println "The arguments must be: a target file name and an EDN map.")))))
 
